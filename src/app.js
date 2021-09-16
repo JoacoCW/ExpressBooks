bcd5e0fac947f254ede7946365dd87cc22242651
JoaquinWeiss <joaco.w93@gmail.com>
2021-09-15 21:20:50 -0300

    changed port

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 
 // Configuro servidor
 
-app.set('port', 4000);
+app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
